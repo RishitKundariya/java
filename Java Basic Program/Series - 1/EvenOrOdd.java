80e7d8f8b8b02bde99511092f2e60a5386d2576c
Rishit kundariya <60743923+RishitKundariya@users.noreply.github.com>
2021-05-04 16:46:41 +0530

    Add files via upload

---
+++ Java Basic Program/Series - 1/EvenOrOdd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class EvenOrOdd{
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the number :- ");
+		int n=sc.nextInt();
+		if (n%2==0) 
+			System.out.println("Number "+n+" is Even.");
+		else
+			System.out.println("Number "+n+" is Odd.");
+	}
+}
