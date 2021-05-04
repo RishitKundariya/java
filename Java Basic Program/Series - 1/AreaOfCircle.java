5975dd06ac692333022c54a8020e8fafc7fbb600
Rishit kundariya <60743923+RishitKundariya@users.noreply.github.com>
2021-05-04 16:46:01 +0530

    Add files via upload

---
+++ Java Basic Program/Series - 1/AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AreaOfCircle{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Radius of circle :- ");
+		double r=sc.nextDouble();
+		System.out.println("Area of circle :-"+(3.14*r*r));
+
+	}
+}
